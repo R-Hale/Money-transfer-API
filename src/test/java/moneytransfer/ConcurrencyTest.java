f72ec96995cb5926b0c812c9b361747edd03f428
Richard <r.merinohale@hotmail.com>
2019-10-09 00:08:54 +0200

    concurrency testing

--- src/test/java/moneytransfer/ConcurrencyTest.java
+++ src/test/java/moneytransfer/ConcurrencyTest.java
@@ -4,9 +4,21 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.io.IOException;
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpPut;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
@@ -26,12 +38,6 @@ public class ConcurrencyTest {
 	
 	protected static HttpClient client ;
 	protected static PoolingHttpClientConnectionManager connManager = new PoolingHttpClientConnectionManager();
-	private static int TEST_WITHDRAW_ACCOUNT_ID;
-	private static int TEST_ACCOUNT_ID_SENDER;
-	private static int TEST_ACCOUNT_ID_RECEIVER;
-	private static String TEST_ACCOUNT_WITHDRAW_NAME = "Andrew Johnson";
-	private static String TEST_ACCOUNT_SENDER_NAME = "John Johnson";
-	private static String TEST_ACCOUNT_RECEIVER_NAME = "Jane Johnson";
 	private static String url;
 	private static int port;
 	
@@ -46,127 +52,136 @@ public class ConcurrencyTest {
 		catch(Exception e) {
 			System.out.println("Bind address already in use");
 		}
-
-		initialiseDao();
-		CloseableHttpClient httpClient = HttpClients.createDefault();
-		final HttpPut httpPutInit = new HttpPut(url + port + "/accounts/" + TEST_WITHDRAW_ACCOUNT_ID + "/deposit/" + 100);
-		httpClient.execute(httpPutInit);
-		final HttpPut httpPut = new HttpPut(url + port + "/accounts/" + TEST_ACCOUNT_ID_SENDER + "/deposit/" + 100);
-		httpClient.execute(httpPut);
 	}
 	
-	private static void initialiseDao() {
-		AccountDaoDefault accountInstance = DaoInstance.getInstance().getAccountDao();
-		
-		Account testAccountWithdraw = new Account(TEST_ACCOUNT_WITHDRAW_NAME);
-		TEST_WITHDRAW_ACCOUNT_ID = testAccountWithdraw.getId();
-		accountInstance.save(testAccountWithdraw);
-		
-		Account testAccountSender = new Account(TEST_ACCOUNT_SENDER_NAME);
-		TEST_ACCOUNT_ID_SENDER = testAccountSender.getId();
-		accountInstance.save(testAccountSender);
-		
-		Account testAccountReceiver = new Account(TEST_ACCOUNT_RECEIVER_NAME);
-		TEST_ACCOUNT_ID_RECEIVER = testAccountReceiver.getId();
-		accountInstance.save(testAccountReceiver);
-	}
-	
-	@Test
-	public void withdraw1() throws IOException {
-		withdraw(TEST_WITHDRAW_ACCOUNT_ID, BigDecimal.TEN.multiply(BigDecimal.TEN));
-	}
-	
-	@Test
-	public void withdraw2() throws IOException {
-		withdraw(TEST_WITHDRAW_ACCOUNT_ID, BigDecimal.TEN.multiply(BigDecimal.TEN));
-	}
-	
-	@Test
-	public void withdraw3() throws IOException {
-		withdraw(TEST_WITHDRAW_ACCOUNT_ID, BigDecimal.TEN.multiply(BigDecimal.TEN));
-	}
-	
-	@Test
-	public void withdraw4() throws IOException {
-		withdraw(TEST_WITHDRAW_ACCOUNT_ID, BigDecimal.TEN.multiply(BigDecimal.TEN));
-	}
-	
-	@Test
-	public void withdraw5() throws IOException {
-		withdraw(TEST_WITHDRAW_ACCOUNT_ID, BigDecimal.TEN.multiply(BigDecimal.TEN));
-	}
-	
-	
-	@Test
-	public void transfer1() throws IOException {
-		transfer(TEST_ACCOUNT_ID_SENDER, TEST_ACCOUNT_ID_RECEIVER, BigDecimal.TEN.multiply(BigDecimal.TEN));
-	}
-	
-	@Test
-	public void transfer2() throws IOException {
-		transfer(TEST_ACCOUNT_ID_SENDER, TEST_ACCOUNT_ID_RECEIVER, BigDecimal.TEN.multiply(BigDecimal.TEN));
-	}
-	
-	@Test
-	public void transfer3() throws IOException {
-		transfer(TEST_ACCOUNT_ID_SENDER, TEST_ACCOUNT_ID_RECEIVER, BigDecimal.TEN.multiply(BigDecimal.TEN));
-	}
-	
-	@Test
-	public void transfer4() throws IOException {
-		transfer(TEST_ACCOUNT_ID_SENDER, TEST_ACCOUNT_ID_RECEIVER, BigDecimal.TEN.multiply(BigDecimal.TEN));
-	}
-	
-	@Test
-	public void transfer5() throws IOException {
-		transfer(TEST_ACCOUNT_ID_SENDER, TEST_ACCOUNT_ID_RECEIVER, BigDecimal.TEN.multiply(BigDecimal.TEN));
-	}
-	
-	@Test
-	public void transfer6() throws IOException {
-		transfer(TEST_ACCOUNT_ID_SENDER, TEST_ACCOUNT_ID_RECEIVER, BigDecimal.TEN.multiply(BigDecimal.TEN));
-	}
-	
-	@Test
-	public void transfer7() throws IOException {
-		transfer(TEST_ACCOUNT_ID_SENDER, TEST_ACCOUNT_ID_RECEIVER, BigDecimal.TEN.multiply(BigDecimal.TEN));
-	}
-	
-	@Test
-	public void transfer8() throws IOException {
-		transfer(TEST_ACCOUNT_ID_SENDER, TEST_ACCOUNT_ID_RECEIVER, BigDecimal.TEN.multiply(BigDecimal.TEN));
-	}
-	
-	@Test
-	public void transfer9() throws IOException {
-		transfer(TEST_ACCOUNT_ID_SENDER, TEST_ACCOUNT_ID_RECEIVER, BigDecimal.TEN.multiply(BigDecimal.TEN));
-	}
-	
-	@Test
-	public void transfer10() throws IOException {
-		transfer(TEST_ACCOUNT_ID_SENDER, TEST_ACCOUNT_ID_RECEIVER, BigDecimal.TEN.multiply(BigDecimal.TEN));
-	}
-	
-	private void withdraw(int withdrawAccountId, BigDecimal withdrawAmount) throws ClientProtocolException, IOException {
+	private CloseableHttpResponse withdraw(int withdrawAccountId, BigDecimal withdrawAmount) throws ClientProtocolException, IOException {
 		CloseableHttpClient httpClient = HttpClients.createDefault();
 		final HttpPut httpPut = new HttpPut(url + port + "/accounts/" + withdrawAccountId + "/withdraw/" + withdrawAmount);
-	    httpClient.execute(httpPut);
+	    return httpClient.execute(httpPut);
 	}
 	
-	private void transfer(int fromAccountId, int tooAccountId, BigDecimal transferAmount) throws ClientProtocolException, IOException {
-		TestingUtils.transfer(fromAccountId, tooAccountId, transferAmount);
+	@Test
+	public void testSynchronousWithdraw() throws ClientProtocolException, IOException, InterruptedException, ExecutionException {
+		AccountDaoDefault accountInstance = DaoInstance.getInstance().getAccountDao();
+		
+		String withdrawAccountName = "Jane Johnson";
+		
+		Account testAccount = new Account(withdrawAccountName);
+		int testAccountId = testAccount.getId();
+		accountInstance.save(testAccount);
+		
+		CloseableHttpClient httpClient = HttpClients.createDefault();
+		final HttpPut httpPutInit = new HttpPut(url + port + "/accounts/" + testAccountId + "/deposit/" + 100);
+		httpClient.execute(httpPutInit);
+		
+		CountDownLatch latch = new CountDownLatch(1);
+		AtomicBoolean running = new AtomicBoolean();
+		AtomicInteger overlapingThreadsCount = new AtomicInteger();
+		int threads = 10;
+	    ExecutorService service = Executors.newFixedThreadPool(threads);
+	    
+	   Collection<Future<CloseableHttpResponse>> futures = new ArrayList<>(threads);
+		for (int t = 0; t < threads; ++t) {
+			futures.add(
+		    service.submit(
+		      () -> {
+		    	  CloseableHttpResponse response = null;
+		    	  try {
+						latch.await();
+				        if (running.get()) {
+				        	overlapingThreadsCount.incrementAndGet();
+				        }
+				        running.set(true);
+				        response = withdraw(testAccountId, BigDecimal.TEN.multiply(BigDecimal.TEN));
+				        running.set(false);
+				        
+		    	  }
+		    	  catch(InterruptedException | IOException e) {
+		    		  e.printStackTrace();
+		    	  }
+		    	  
+		        return response;
+		      }
+		    )
+		  );
+		}
+		latch.countDown();
+		Set<CloseableHttpResponse> ids = new HashSet<>();
+		for (Future<CloseableHttpResponse> f : futures) {
+		  ids.add(f.get());
+		}
+		assertTrue(overlapingThreadsCount.get() > 0);
+
+		BigDecimal testAccountBalance = TestingUtils.getAccountBalance(testAccountId);
+		assertTrue(testAccountBalance.compareTo(new BigDecimal("0")) == 0);
+	}
+	
+	@Test
+	public void testSynchronousTransfers() throws ClientProtocolException, IOException, InterruptedException, ExecutionException {
+		AccountDaoDefault accountInstance = DaoInstance.getInstance().getAccountDao();
+		
+		String senderAccountName = "John Johnson";
+		String receiverAccountName = "Andrew Johnson";
+		
+		Account testAccountSender = new Account(senderAccountName);
+		int senderAccountId = testAccountSender.getId();
+		accountInstance.save(testAccountSender);
+		
+		Account testAccountReceiver = new Account(receiverAccountName);
+		int receiverAccountId = testAccountReceiver.getId();
+		accountInstance.save(testAccountReceiver);
+		
+		CloseableHttpClient httpClient = HttpClients.createDefault();
+		final HttpPut httpPutInit = new HttpPut(url + port + "/accounts/" + senderAccountId + "/deposit/" + 100);
+		httpClient.execute(httpPutInit);
+		
+		CountDownLatch latch = new CountDownLatch(1);
+		AtomicBoolean running = new AtomicBoolean();
+		AtomicInteger overlapingThreadsCount = new AtomicInteger();
+		
+		int threads = 10;
+	    ExecutorService service = Executors.newFixedThreadPool(threads);
+	    
+	   Collection<Future<CloseableHttpResponse>> futures = new ArrayList<>(threads);
+		for (int t = 0; t < threads; ++t) {
+			futures.add(
+		    service.submit(
+		      () -> {
+		    	  CloseableHttpResponse response = null;
+		    	  try {
+						latch.await();
+				        if (running.get()) {
+				        	overlapingThreadsCount.incrementAndGet();
+				        }
+				        running.set(true);
+				        response = TestingUtils.transfer(senderAccountId, receiverAccountId, BigDecimal.TEN.multiply(BigDecimal.TEN));
+				        running.set(false);
+				        
+		    	  }
+		    	  catch(InterruptedException | IOException e) {
+		    		  e.printStackTrace();
+		    	  }
+		    	  
+		        return response;
+		      }
+		    )
+		  );
+		}
+		latch.countDown();
+		Set<CloseableHttpResponse> ids = new HashSet<>();
+		for (Future<CloseableHttpResponse> f : futures) {
+		  ids.add(f.get());
+		}
+		assertTrue(overlapingThreadsCount.get() > 0);
+
+		BigDecimal fromAccountBalance = TestingUtils.getAccountBalance(senderAccountId);
+		BigDecimal toAccountBalance = TestingUtils.getAccountBalance(receiverAccountId);
+		assertTrue(fromAccountBalance.compareTo(new BigDecimal("0")) == 0);
+		assertTrue(toAccountBalance.compareTo(new BigDecimal("100")) == 0);
 	}
 	
 	@AfterAll
 	private static void stopService() throws Exception {
-		BigDecimal withdrawAccountBalance = TestingUtils.getAccountBalance(TEST_WITHDRAW_ACCOUNT_ID);
-		assertTrue(withdrawAccountBalance.compareTo(new BigDecimal("0")) == 0);
-		
-		BigDecimal fromAccountBalance = TestingUtils.getAccountBalance(TEST_ACCOUNT_ID_SENDER);
-		BigDecimal toAccountBalance = TestingUtils.getAccountBalance(TEST_ACCOUNT_ID_RECEIVER);
-		assertTrue(fromAccountBalance.compareTo(new BigDecimal("0")) == 0);
-		assertTrue(toAccountBalance.compareTo(new BigDecimal("100")) == 0);
-		
 		TestingUtils.stopService();
 	}
 
